24a392881eb6ad66762c5d53f792609f8bf8ad15
josepereza <josepereza66@gmail.com>
2025-08-06 18:10:58 +0200

    first commit3

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -2,10 +2,11 @@ import { Component, Input, Pipe } from '@angular/core';
 import { Product } from '../../interfaces/product';
 import { CartService } from '../../services/cart-service';
 import { DecimalPipe } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-product-card',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, NgOptimizedImage],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css'
 })
